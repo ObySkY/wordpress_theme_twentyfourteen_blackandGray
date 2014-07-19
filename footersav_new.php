b2dfdaefacb15e57eb69daaf7f76e5d83cd1994b
ObY <obcd.nicolas@gmail.com>
2014-07-19 22:56:53 +0200

    first commit full operational with four theme.

---
+++ footersav_new.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+			<?php get_sidebar( 'footer' ); ?>
+
+			<div class="site-info">
+
+				<center><a target="_obysky" href="<?php echo 'http://www.obysky.com'; ?>"><?php echo 'Copyright by ObYsKy : 2014 - '.date('Y'); ?></a></center>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
